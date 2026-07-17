cf6e9c17bec8452d5b5785670b88427266d9718b
diegodiazchaverri-byte <diegodiazchaverri@gmail.com>
2026-07-16 18:05:34 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: hace la app instalable y deja el "cascarón" disponible offline.
    Si actualizás index.html, subí también este archivo cambiando la VERSION. */
-const VERSION = 'v7';
+const VERSION = 'v9';
 const CACHE = 'finanzas-' + VERSION;
 const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
